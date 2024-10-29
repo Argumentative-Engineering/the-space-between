6715861cb46f4b9e058d9280340f35ddc6935678
zsfer <seiferalbacete@gmail.com>
2024-10-30 00:40:32 +0800

    feat(game): start work on scene 2

--- Assets/_Features/Misc/Scripts/WorldRotator.cs
+++ Assets/_Features/Misc/Scripts/WorldRotator.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class WorldRotator : MonoBehaviour
 {
+    [SerializeField] Transform _moon;
+    [SerializeField] Transform _light;
     [SerializeField] float _rotationSpeed = 2;
     [SerializeField] float _moonRotationSpeed = 2;
     [SerializeField] Material _skybox;
 
     float _rot = 0;
 
+    void Start()
+    {
+        _rot = transform.localEulerAngles.y;
+    }
+
     void Update()
     {
         _rot += Time.deltaTime;
         _skybox.SetFloat("_Rotation", (-_rot * _rotationSpeed % 360) + 180);
-        transform.rotation = Quaternion.Euler(0, _rot * _moonRotationSpeed, 0);
+        _light.rotation = Quaternion.Euler(0, _rot * _rotationSpeed, 0);
+        _moon.rotation = Quaternion.Euler(0, _rot * _moonRotationSpeed, 0);
     }
 }
