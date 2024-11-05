ee102f18a687edf40a68fe7bf17b3666b4481dd6
zsfer <seiferalbacete@gmail.com>
2024-11-05 22:27:47 +0800

    fix: removed fixed joint on grabbables

--- Assets/_Features/Puzzle/Scripts/GrabbableObject.cs
+++ Assets/_Features/Puzzle/Scripts/GrabbableObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(FixedJoint))]
+// [RequireComponent(typeof(FixedJoint))]
 public class GrabbableObject : GameInteractable
 {
     [SerializeField] Transform _anchor;
